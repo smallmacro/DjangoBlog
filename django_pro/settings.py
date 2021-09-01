2f96b73fe04006696c700a12ea756258ecf6fc52
smallmacro <smallmacro@gmail.com>
2021-09-01 10:59:35 +0800

    Update new feature: User can modify User Profile,displayding profile images in home page

--- django_pro/settings.py
+++ django_pro/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'users.apps.UsersConfig',
     'blog.apps.BlogConfig',
-    'crispy_forms',
+    'crispy_forms',      
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- users/models.py
+++ users/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from PIL import Image
 
 # Create your models here.
 class Profile(models.Model):
@@ -10,3 +10,14 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        size = 300, 300
+
+        with Image.open(self.image.path) as img:
+            if img.height > 300 or img.width > 300:
+                img.thumbnail(size)
+                img.save(self.image.path)
+        
