e222e4e236f2284aca40b6158f234782deb06c04
smallmacro <smallmacro@gmail.com>
2021-09-01 08:38:22 +0800

    Update User Profile and Picture:Fix the media path issue by correct the settings.py

--- django_pro/settings.py
+++ django_pro/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDUA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'blog:blog-home'    #customize the redirect url after login
