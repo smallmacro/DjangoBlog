9984a5925c7bed90925b2ad6e9378b82d13a1b73
smallmacro <smallmacro@gmail.com>
2021-09-07 14:03:14 +0800

    Update STATIC ROOT

--- django_pro/settings.py
+++ django_pro/settings.py
@@ -121,6 +121,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+#Set ROOT static for heroku
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
