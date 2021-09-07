d84f448bafe31be77f4414f69c5f4b3d5fe71d20
smallmacro <smallmacro@gmail.com>
2021-09-07 15:30:17 +0800

    Update ALLOWED_HOSTS

--- django_pro/settings.py
+++ django_pro/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'c2w8+bi*2n#!l3x_(nu7f)tclw@(bxaj@blg$3z6!1771p6_*('
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['trydjangoapp.herokuapp.com']
 
 
 # Application definition
